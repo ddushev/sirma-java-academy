a3f2143dad5f23c9490cbbdbfee637d1a68bea42
Daniel Dushev <danieldyshew@gmail.com>
2024-05-14 10:39:13 +0300

    DataTypes. add class

---
+++ basic-syntax/src/DataTypes.java
@@ -0,0 +1,8 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int intNum = 100;
+        double dblNum = 3.15;
+        System.out.println(intNum);
+        System.out.println(dblNum);
+    }
+}
