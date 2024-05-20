fe691752a39400ad8602133071ac5dddd2024407
Daniel Dushev <danieldyshew@gmail.com>
2024-05-20 17:35:52 +0300

    ReverseArrayOfNumbers. convert arrays to integers

--- arrays/src/ReverseArrayOfNumbers.java
+++ arrays/src/ReverseArrayOfNumbers.java
@@ -6,12 +6,13 @@ public class ReverseArrayOfNumbers {
     Scanner sc = new Scanner(System.in);
 
     int n = Integer.parseInt(sc.nextLine());
-    String[] arrInput = sc.nextLine().split(",");
-    String[] newArr = new String[n];
+    int[] arrInput = Arrays.stream(sc.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+    int[] newArr = new int[n];
 
     for (int i = 0; i < n; i++) {
       newArr[i] = arrInput[n - i - 1];
     }
-    System.out.println(String.join(" ", newArr));
+
+    System.out.println(Arrays.toString(newArr));
   }
 }
