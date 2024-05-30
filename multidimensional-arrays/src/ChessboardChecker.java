6b1dc212e7d410af9adb577967d39e88e36edc83
Daniel Dushev <danieldyshew@gmail.com>
2024-05-30 17:23:30 +0300

    ChessboardChecker. solved

--- multidimensional-arrays/src/ChessboardChecker.java
+++ multidimensional-arrays/src/ChessboardChecker.java
@@ -2,14 +2,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ChessboardChecker {
-  static void printMatrix(int[][] matrix) {
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[i].length; j++) {
-        System.out.print(matrix[i][j] + " ");
-      }
-      System.out.println();
-    }
-  }
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -71,6 +63,5 @@ public class ChessboardChecker {
 
     }
     System.out.println(isThreaten);
-    printMatrix(chessBoard);
   }
 }
